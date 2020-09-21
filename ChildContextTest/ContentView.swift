314f2ccc51737a6e3cbe6e4837c2c85a8ab19a0f
aaronium112 <aaron@sphericalwave.com>
2020-09-21 14:52:38 -0300

    Initial Commit

---
+++ ChildContextTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChildContextTest
+//
+//  Created by Aaron Anthony on 2020-09-21.
+//  Copyright © 2020 SphericalWaveSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
